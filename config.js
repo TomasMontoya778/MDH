e7982bf816e97f0dea76608a812400e3c95064cc
Tomas Montoya <tomas1213monto@gmail.com>
2025-02-04 20:36:14 -0500

    corrigiendo

--- config.js
+++
@@ -1,2 +0,0 @@
-const API_URL = "https://app-3e112ab4-f0df-498c-9cb3-6948901fb7bc.cleverapps.io/proyecto/backend";
-export default API_URL;
--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-import API_URL from "./config.js";
-
+const API_URL = "https://app-3e112ab4-f0df-498c-9cb3-6948901fb7bc.cleverapps.io/proyecto/backend";
 $(document).ready(function(){
     $("#btnMenu").click(function(){
         $("#navbarNav").toggleClass("show");
